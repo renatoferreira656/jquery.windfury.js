e3ea811a6614abd2b2c3ac8a6f7c44b7c4776d9c
murer <murer@dextra-sw.com>
2014-03-11 09:09:47 -0300

    tests

--- src/main/webapp/js/jquery.windfury.js
+++ src/main/webapp/js/jquery.windfury.js
@@ -1,11 +1,22 @@
 (function($) {
 
-	function windfury(text) {
-		return function() {
-			return text;
-		}
+	function windfury(text, callback) {
 	}
-	
+
+	function getWindfury(url, data, callback) {
+		if(typeof(data) == 'function') {
+			callback = data;
+			data = null;
+		}
+		return $.ajax({
+			url : url,
+			dataType : 'windfury',
+			data: data,
+			success : callback
+		});
+	}
+
 	$.windfury = windfury;
-	
+	$.getWindfury = getWindfury;
+
 })(jQuery);
