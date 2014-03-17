36b835e5ae72bf8e8159788617005db649e3a653
murer <murer@dextra-sw.com>
2014-03-17 08:00:53 -0300

    tests

--- src/main/webapp/js/jquery.windfury.js
+++ src/main/webapp/js/jquery.windfury.js
@@ -1,31 +1,5 @@
 (function($) {
 
-	function executeWindfury(text, callback) {
-		var doc = $.parseXML(text);
-		return parse(doc, callback);
-	}
-
-	function getWindfury(url, data, callback) {
-		if (typeof (data) == 'function') {
-			callback = data;
-			data = null;
-		}
-		return $.ajax({
-			url : url,
-			dataType : 'windfury',
-			data : data,
-			success : callback
-		});
-	}
-
-	function readText(element) {
-		var ret = [];
-		$.each($(element).contents(), function(idx, child) {
-			ret.push(readChildrenText(child));
-		});
-		return ret.join('');
-	}
-
 	function readChildrenText(element) {
 		var ret = [];
 		if (element.nodeType == 1) {
@@ -46,7 +20,29 @@
 		return ret.join('');
 	}
 
-	function parse(xml, callback) {
+	function readText(element) {
+		var ret = [];
+		$.each($(element).contents(), function(idx, child) {
+			ret.push(readChildrenText(child));
+		});
+		return ret.join('');
+	}
+
+	function secureEval(code, xml, success) {
+		var windfury = {};
+		for ( var i in parseWindfury.spec) {
+			var spec = parseWindfury.spec[i];
+			windfury[i] = spec({
+				wf : windfury,
+				xml : xml,
+				success : success
+			});
+		}
+
+		eval(code);
+	}
+
+	function parse(xml, success) {
 		xml = $(xml).children();
 		if (!xml.is('.windfury')) {
 			throw 'root node must be .windfury';
@@ -56,23 +52,16 @@
 			throw 'you must write one <script/>';
 		}
 		var scriptCode = readText(scripts);
-		secureEval(scriptCode, xml, callback);
+		secureEval(scriptCode, xml, success);
 	}
 
-	function secureEval(code, xml, callback) {
-		var windfury = {};
-		for ( var i in executeWindfury.spec) {
-			var spec = executeWindfury.spec[i];
-			windfury[i] = spec({
-				wf : windfury,
-				xml : xml,
-				callback : callback
-			});
+	function parseWindfury(doc, success) {
+		if (typeof (doc) == 'string') {
+			doc = $.parseXML(doc);
 		}
-
-		eval(code);
+		parse(doc, success);
 	}
-
+	
 	function wfRead(ctx) {
 		return function(path) {
 			return $.trim(readText(ctx.xml.children('section').filter(path)));
@@ -90,130 +79,33 @@
 
 	function wfDef(ctx) {
 		return function(obj) {
-			if (ctx.callback) {
-				ctx.callback(obj);
+			if (ctx.success) {
+				ctx.success(obj);
 			}
 		}
 	}
-
-	executeWindfury.spec = {};
-	executeWindfury.spec.read = wfRead;
-	executeWindfury.spec.text = wfText;
-	executeWindfury.spec.def = wfDef;
-
-	$.ajaxSetup({
-		converters : {
-			'xml windfury' : true
+	
+	function getWindfury(url, data, success) {
+		if (typeof (data) == 'function') {
+			success = data;
+			data = null;
 		}
-	});
-
-	$.ajaxPrefilter(function(options, originalOpts, jqXHR) {
-		var dataType = originalOpts.dataType;
-		if (dataType && dataType === 'windfury') {
-			var callback = options.success;
-			var wfCallback = function(doc) {
-				parse(doc, function(obj) {
-					if (callback) {
-						callback(obj);
-					}
-				});
-			};
-			options.success = wfCallback;
-			return 'xml';
-		}
-	});
-
-	function wf(names, callback) {
-		if (typeof (names) == 'string') {
-			return wf([ names ], callback);
-		}
-		if (!names || !names.length) {
-			return;
-		}
-
-		var map = {};
-		for (var i = 0; i < names.length; i++) {
-			var name = names[i];
-			map[name] = wf.loads[name] || {
-				name : name,
-				loading : true
-			};
-		}
-
-		function makeCallback(map, callback) {
-			if(!callback) {
-				return;
+		return $.ajax({
+			url : url,
+			dataType : 'xml',
+			data : data,
+			success : function(xml) {
+				$.windfury(xml, success);
 			}
-			var resps = [];
-			for ( var name in map) {
-				var entry = map[name];
-				resps.push(entry.obj);
-			}
-			callback.apply(window, resps);
-		}
-
-		function setSuccess(entry) {
-			return function(obj) {
-				entry.obj = obj;
-			}
-		}
-
-		function setError(entry) {
-			return function(error, type, msg) {
-				console.info('error', arguments);
-				entry.error = {
-					error : error,
-					type : type,
-					msg : msg
-				};
-			}
-		}
-
-		function checkComplete(map) {
-			for ( var name in map) {
-				var entry = map[name];
-				if (entry.loading) {
-					return;
-				}
-			}
-			makeCallback(map, callback);
-		}
-
-		function setComplete(entry, map) {
-			return function() {
-				entry.loading = false;
-				wf.loads[entry.name] = entry;
-				checkComplete(map);
-			}
-		}
-
-		function process(map) {
-			var isLoading = false;
-			for ( var name in map) {
-				var entry = map[name];
-				if (entry.loading) {
-					$.ajax({
-						url : entry.name,
-						dataType : 'windfury',
-						success : setSuccess(entry),
-						complete : setComplete(entry, map),
-						error : setError(entry)
-					});
-					isLoading = true
-				}
-			}
-			if (!isLoading) {
-				makeCallback(map, callback);
-			}
-		}
-
-		process(map);
-
+		});
 	}
-	wf.loads = {};
 
-	$.wf = wf;
-	$.windfury = executeWindfury;
+	parseWindfury.spec = {};
+	parseWindfury.spec.read = wfRead;
+	parseWindfury.spec.text = wfText;
+	parseWindfury.spec.def = wfDef;
+	
+	$.windfury = parseWindfury;
 	$.getWindfury = getWindfury;
 
 })(jQuery);
--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -1,20 +1,9 @@
 (function($) {
 	
 	$(window).ready(function() {
-//		$.getWindfury('template/main.html', function(main) {
-//			console.info('x', main);
-//			$('#main').html(main);
-//		});
-		$.ajax({
-			url: 'template/main.html',
-			dataType: 'windfury',
-			success: function(main) {
-				$('#main').html(main);	
-			},
-			error: function() {
-				console.info('error', arguments);
-			}
-		})
+		$.getWindfury('template/main.html', function(main) {
+			$('#main').html(main);
+		});
 	});
 	
 })(jQuery);
