a792eac9c5f7631a9e0bdf41fe86a50c875e67e1
Renato Ferreira Soares <renato.soares@dextra-sw.com>
2015-10-28 20:58:56 -0200

    adding tests

--- src/main/webapp/test/cases/templateTest.js
+++ src/main/webapp/test/cases/templateTest.js
@@ -1,22 +1,61 @@
 (function($, t) {
 
-	var code = '';
-	code += '<section class="windfury">';
-	code += '<section class="text">my text template</section>';
-	code += '<section class="dot">my dot template: {{=it}}</section>';
-	code += '<script>windfury.def({';
-	code += 'read: windfury.read(".text"),';
-	code += 'text: windfury.text(".text"),';
-	code += 'dot: windfury.doT(".dot")';
-	code += '});</script></section>';
 
 	t.module("templateTest");
 
 	t.test('template test', function() {
+		var code = "<section class='windfury'> \
+			<section class='text'>my text template</section> \
+			<section class='dot'>my dot template: {{=it}}</section> \
+			<script type='application/javascript'> \
+				windfury.def({ \
+					read: windfury.read('.text'), \
+					text: windfury.text('.text'), \
+					dot: windfury.doT('.dot') \
+				}); \
+			</script> \
+		</section>";
+
 		var obj = $.windfury(code);
 		equal(obj.read, 'my text template');
 		equal(obj.text(), 'my text template');
 		equal(obj.dot(2), 'my dot template: 2');
+	});
+
+	t.test('template test with non xml attributes', function() {
+		var code = "<section class='windfury'> \
+			<section class='dot'>my dot template: {{=it}}</section> \
+			<script type='text/javascript'> \
+				function text(){ \
+					var template = windfury.doT('.dot'); \
+					return template('&'); \
+				} \
+				windfury.def({ \
+					dot: text \
+				}); \
+			</script> \
+		</section>";
+
+		var obj = $.windfury(code);
+		equal(obj.dot(), 'my dot template: &');
+	});
+
+	t.test('template test with dumb type', function() {
+		var code = "<section class='windfury'> \
+			<section class='dot'>my dot template: {{=it}}</section> \
+			<script language='bla'> \
+				function text(){ \
+					var template = windfury.doT('.dot'); \
+					return template('&'); \
+				} \
+				windfury.def({ \
+					dot: text \
+				}); \
+			</script> \
+		</section>";
+
+		var obj = $.windfury(code);
+		equal(obj.dot(), 'my dot template: &');
 	});
 
 })(jQuery, QUnit);
